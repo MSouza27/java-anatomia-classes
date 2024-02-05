00b981386375b0630f8f1ccc962db10d2964388c
Magno <magnoapt@gmail.com>
2024-02-04 21:11:26 -0300

    Primeira Semana

---
+++ src/edu/magno/primeirasemana/Boletim.java
@@ -0,0 +1,19 @@
+package edu.magno.primeirasemana;
+
+public class Boletim {
+
+    public static void main(String[] args) {
+    
+        int mediaFinal = 6;
+
+
+        //Estrutura condicional
+        if(mediaFinal<6){
+            System.out.println("Reprovado");
+        }else if(mediaFinal==6){
+                System.out.println("Prova Minerva");
+        }  else{
+                System.out.println("Aprovado");
+            }
+        }   
+}
